3f44f43fcb2bd04eadfc5189b490b2ae67ba28dd
Stephanie Temmerman <greytem@hotmail.com>
2021-03-12 09:24:47 -0800

    header and main changes for arrow

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "./Button";
+import arrow from "../assets/icons/arrow.svg";
 
 function Header(props) {
   return (
@@ -9,13 +10,17 @@ function Header(props) {
         <a className="site-header__brand" href="/">
           <img className="site-header__logo" src={props.logo} alt="clio logo" />
         </a>
-          <p className="site-header__navTitle">Products V</p>
-          <p className="site-header__navTitle">Solutions V</p>
+        <div className="column__one">
+          <p className="site-header__navTitle">Products<img className="arrow" src={arrow} alt="arrow"/> </p>
+          <p className="site-header__navTitle">Solutions <img  className="arrow" src={arrow} alt="arrow"/> </p>
           <p className="site-header__navTitle">Pricing </p>
-          <p className="site-header__navTitle">Customer Stories</p>
-          <p className="site-header__navTitle">Resources V</p>
-          <p className="site-header__navTitle">Log in V</p>
+          <p className="site-header__navTitle ">Customer Stories</p>
+          <p className="site-header__navTitle">Resources<img className="arrow" src={arrow} alt="arrow"/>  </p>
+          </div>
+          <div className="column__two">
+          <p className="site-header__navTitle">Log in<img className="arrow" src={arrow} alt="arrow"/>  </p>
           <Button text="Try it Free" className="site-header__try" />
+          </div>
         </div>                          
       </header>
     </div>
--- src/components/MainNavBar.jsx
+++ src/components/MainNavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from "./Button";
+import arrow from "../assets/icons/arrow.svg";
 
 function MainNavBar() {
     return (
@@ -7,19 +8,19 @@ function MainNavBar() {
     
             <div className="main-nav__container">
                 <label htmlFor="comment" className="main-nav__formlabel">Show results for</label>
-                <input id="comment" className="main-nav__forminput" name="main-nav__description" placeholder="Law firms   V"></input>
+                <input id="comment" className="main-nav__forminput" name="main-nav__description" placeholder="Law firms "/><i  className="fa fa-chevron" src={arrow} alt="arrow"/>
             </div>  
             <div className="main-nav__container">
                 <label htmlFor="comment" className="main-nav__formlabel">Area of Practice</label>
-                <input id="comment" className="main-nav__forminput" name="main-nav__description" placeholder="Family Law   V"></input>
+                <input id="comment" className="main-nav__forminput" name="main-nav__description" placeholder="Family Law  "/><i className="fa fa-chevron" src={arrow} alt="arrow"/>
             </div>      
                 <div className="main-nav__container">
                 <label htmlFor="comment" className="main-nav__formlabel">Area of Specialization</label>
-                <input id="comment" className="main-nav__forminput" name="main-nav__description" placeholder="Specialization   V"></input>
+                <input id="comment" className="main-nav__forminput" name="main-nav__description" placeholder="Specialization"/><i className="fa fa-chevron" src={arrow} alt="arrow"/>
                 </div> 
             <div className="main-nav__container">
                 <label htmlFor="comment" className="main-nav__formlabel">Location</label>
-                <input id="comment" className="main-nav__forminput" name="main-nav__description" placeholder="Vancouver,BC   V"></input>
+                <input id="comment" className="main-nav__forminput" name="main-nav__description" placeholder="Vancouver,BC"/><i className="fa fa-chevron" src={arrow} alt="arrow"/>
             </div> 
             <Button text="Search" className="main-nav__search" />
         </div>
